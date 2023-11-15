7fd5b235de8eb7bd4816f29f036b9ab3a278ee8c
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-11-15 13:30:08 -0500

    qw

--- main.ts
+++ main.ts
@@ -5,4 +5,38 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let loopCounter = 0
+let loopCounter2 = 0
+let mySprite: game.LedSprite = null
+
+// setup
+basic.pause(500)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// on a button pressed, make the pixels light up around the edge
+input.onButtonPressed(Button.A, function () {
+mySprite = game.createSprite(0, 0)
+
+  // x axis for top half
+  while (loopCounter <= 5) {
+    mySprite.set(LedSpriteProperty.X, loopCounter)
+    basic.pause(500)
+    loopCounter++
+
+    // start y axis for the right side
+    if (loopCounter <= 5) {
+      loopCounter2++
+    }
+    // y axis for right side
+    while (loopCounter2 <= 5) {
+        mySprite.set(LedSpriteProperty.X, loopCounter)
+        basic.pause(500)
+        loopCounter2++
+    }
+}
+
+mySprite.delete()
+basic.pause(500)
+})
